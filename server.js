ca43b80d92ea0253b81c075bfb4412791f1c67a2
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2017-09-05 13:28:32 -0700

    fixed handlebars

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var models = require("./models");
+// var bodyParser = require("body-parser");
+// var models = require("/models");
 var exphbs  = require('express-handlebars');
 var mongojs = require("mongojs");
 var request = require("request");
 var cheerio  = require("cheerio");
-var mongodb = require("mongodb");
+// var mongodb = require("mongodb");
 var app = express();
-var databaseUrl = "newsDB";
+var databaseUrl = "news";
 var collections = ["articles"];
 
 var db = mongojs(databaseUrl, collections);
@@ -15,58 +15,46 @@ var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
   console.log("Database Error:", error);
 });
+app.use(express.static(__dirname + "/public"));
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
 
 app.get("/", function(req, res) {
-  res.send("Hello world");
+  res.render("index");
 });
 
-app.get("/all", function(req, res) {
-  db.articles.find({}, function(error, found) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(found);
-    }
-  });
-});
+// app.get("/all", function(req, res) {
+//   db.articles.find({}, function(error, found) {
+//     if (error) {
+//       console.log(error);
+//     }
+//     else {
+//       res.json(found);
+//     }
+//   });
+// });
 
-app.get("/scrape", function(req, res) {
-  // Make a request for the news section of ycombinator
-  request("https://sfchronicle.com/", function(error, response, html) {
-    // Load the html body from request into cheerio
-    var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $(".headline").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children("a").text();
-      var link = $(element).children("a").attr("href");
+// app.get("/scrape", function(req, res) {
+//   // Make a request for the news section of ycombinator
+//   request("https://sfchronicle.com/", function(error, response, html) {
+//     // Load the html body from request into cheerio
+//     var $ = cheerio.load(html);
+//     var results = [];
+//     // For each element with a "title" class
+//     $("h2.headline").each(function(i, element) {
+//       // Save the text and href of each link enclosed in the current element
+//       var title = $(element).children().text();
+//       var link = $(element).children("a").attr("href");
+//      results.push({
+//       title: title,
+//       link: link
+//      });
+//    });
 
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the articles db
-        db.articles.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
-    });
-  });
-
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
-
+// console.log(results);
+// });
+// });
 
 // Listen on port 3000
 app.listen(3000, function() {
