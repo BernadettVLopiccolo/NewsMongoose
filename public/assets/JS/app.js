cc2ee1bf872fa58f9f287b128333d6af6a90b065
Bernadett Villam Lopiccolo <bernadettvillam@yahoo.com>
2017-09-10 13:41:02 -0700

    added functions to save article, comment a saved article, and delete saved article. Have to fix functionality on all. Delete comment function is still missing

--- public/assets/JS/app.js
+++ public/assets/JS/app.js
@@ -1,47 +1,62 @@
 $(document).ready(function() {
-	$.getJSON("/articles", function(data) {
-		for (var i = 0; i < data.length; i++) {
-			$("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-		}
-	});
+    $.getJSON("/articles", function(data) {
+        for (var i = 0; i < data.length; i++) {
+            $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><a href='" + data[i].link + "'>" + data[i].link + "</a></p><button type='submit' id='save' class='btn btn-default'>Save Article</button>");
+            // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        }
+    });
 
-	$(document).on("click", "p", function() {
-		$("#comments").empty();
-		var thisId = $(this).attr("data-id");
-		$.ajax({
-			method: "GET",
-			url: "/articles/" + thisId
-		})
-		.done(function(data) {
-			console.log(data);
-			$("#comments").append("<h2>" + data.title + "</h2>");
-			// $("#comments").append("<input id='titleInput' name='title' >");
-			$("#comments").append("<textarea id='bodyInput' name='body'></textarea>");
-			$("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment</button>");
+    $(document).on("click", "#save", function() {
+        var thisId = $(this).attr("data-id");
+        $.ajax({
+                method: "GET",
+                url: "/saved/" + thisId
+            })
+            .done(function(data) {
+                console.log(data);
+                $.getJSON("/saved", function(data) {
+                    for (var j = 0; j < data.length; i++) {
+                        $("#savedArticles").append("<p data-id='" + data[j]._id + "'>" + data[j].title + "<br /><a href='" + data[j].link + "'>" + data[j].link + "</a></p><button type='submit' id='comment' class='btn btn-default'>Comment</button><button>Delete Article</button>");
+                    }
+                });
+            });
+    });
 
-			if (data.comment) {
-				// $("#titleinput").val(data.comment.title);
-				$("#bodyInput").val(data.comment.body);
-			}
+    $(document).on("click", "p", function() {
+        $("#comments").empty();
+        var thisId = $(this).attr("data-id");
+        $.ajax({
+                method: "GET",
+                url: "/saved/" + thisId
+            })
+            .done(function(data) {
+                console.log(data);
+                $("#comments").append("<h2>" + data.title + "</h2>");
+                $("#comments").append("<textarea id='bodyInput' name='body'></textarea>");
+                $("#comments").append("<button data-id='" + data._id + "' id='saveComment'>Save Comment</button>");
 
-		});
-	});
+                if (data.comment) {
+                    $("#bodyInput").val(data.comment.body);
+                }
 
-	$(document).on("click", "saveComment", function() {
-		var thisId = $(this).attr("data-id");
-		$.ajax({
-			method: "POST",
-			url: "/articles/" + thisId,
-			data: {
-				// title: $("#titleinput").val(),
-				body: $("bodyInput").val()
-			}
-		})
-		.done(function(data) {
-			console.log("data", data);
-            $("comments").empty();
-		});
-		// $("#titleInput").val("");
-		$("#bodyInput").val("");
-	});
+            });
+    });
+
+    $(document).on("click", "#saveComment", function() {
+        var thisId = $(this).attr("data-id");
+        $.ajax({
+                method: "POST",
+                url: "/saved/" + thisId,
+                data: {
+                    body: $("bodyInput").val()
+                }
+            })
+            .done(function(data) {
+                console.log("data", data);
+                $("comments").empty();
+            });
+        $("#bodyInput").val("");
+    });
+
+
 });
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 // var mongojs = require("mongojs");
 var request = require("request");
-var cheerio  = require("cheerio");
+var cheerio = require("cheerio");
 var Article = require("./models/article.js");
 var Comment = require("./models/comment.js");
 var logger = require("morgan");
@@ -17,25 +17,27 @@ var app = express();
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 mongoose.connect("mongodb://localhost/newsDB");
 var db = mongoose.connection;
 // var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
-  console.log("Mongoose Error:", error);
+    console.log("Mongoose Error:", error);
 });
 app.use(express.static(__dirname + "/public"));
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 db.once("open", function() {
-  console.log("Mongoose connection succesful.");
+    console.log("Mongoose connection succesful.");
 });
 
 app.get("/", function(req, res) {
-  res.render("index");
+    res.render("index");
 });
 
 app.get("/scrape", function(req, res) {
@@ -51,105 +53,99 @@ app.get("/scrape", function(req, res) {
             result.link = $(this).find("a").attr("href");
             var entry = new Article(result);
             entry.save(function(err, doc) {
-              if (err) {
-                console.log(err);
-              }
-              else {
-                console.log(doc);
-              }
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(doc);
+                }
             });
-            // console.log(title, link);
-            // results.push({
-            //   link: link,
-            //   title: title
-            // });
-            // db.articles.insert({
-            //   title: title,
-            //   link: link
-            // });
 
         });
-        // res.send(results);
+
         res.send("Scrape Complete");
     });
 });
 
 // This will get the articles we scraped from the mongoDB
 app.get("/articles", function(req, res) {
-  Article.find({}, function(error, doc) {
-    if (error) {
-      res.send(error);
-    }
-    else {
-      res.json(doc);
-    }
-  });
+    Article.find({}, function(error, doc) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.json(doc);
+        }
+    });
 });
 
 // Grab an article by it's ObjectId
 app.get("/articles/:id", function(req, res) {
-  Article.findOne({"_id": req.params.id})
-  .populate("comment")
-  .exec(function(error, doc) {
-    if (error) {
-      res.send(error);
-    }
-    else {
-      res.json(doc);
-    }
-  });
+    Article.findOne({
+            "_id": req.params.id
+        })
+        .populate("comment")
+        .exec(function(error, doc) {
+            if (error) {
+                res.send(error);
+            } else {
+                res.json(doc);
+            }
+        });
 });
 
 // Create a new note or replace an existing note
 app.post("/articles/:id", function(req, res) {
-  var newComment = new Comment(req.body);
-  // And save the new note the db
-  newComment.save(function(error, doc) {
-    if (error) {
-      res.send(error);
-    }
-    else {
-      // Use the article id to find and update it's note
-      Article.findOneAndUpdate({"_id": req.params.id}, {"comment": doc._id})
-      .exec(function(err, doc) {
-        if (err) {
-          res.send(err);
+    var newComment = new Comment(req.body);
+    // And save the new note the db
+    newComment.save(function(error, doc) {
+        if (error) {
+            res.send(error);
+        } else {
+            // Use the article id to find and update it's note
+            Article.findOneAndUpdate({
+                    "_id": req.params.id
+                }, {
+                    "comment": doc._id
+                })
+                .exec(function(err, doc) {
+                    if (err) {
+                        res.send(err);
+                    } else {
+                        res.send(doc);
+                    }
+                });
         }
-        else {
-          res.send(doc);
-        }
-      });
-    }
-  });
+    });
 });
 
 app.post("/deleteComment/:id", function(req, res) {
-  console.log(req.params.id);
-  Comment.findIdAndRemove({_id: req.params.id}, function(err) {
-    if (err) {
-      console.log("error deleting comment", err);
-    }
-    else {
-      res.send();
-    }
-  });
+    console.log(req.params.id);
+    Comment.findIdAndRemove({
+        _id: req.params.id
+    }, function(err) {
+        if (err) {
+            console.log("error deleting comment", err);
+        } else {
+            res.send();
+        }
+    });
 });
 
 app.post("/deleteArticle/:id", function(req, res) {
-  console.log(req.params.id);
-  Article.findIdAndRemove({_id: req.params.id}, function(err) {
-    if (err) {
-      console.log("error deleting article", err);
-    }
-    else {
-      res.send();
-    }
-  });
+    console.log(req.params.id);
+    Article.findIdAndRemove({
+        _id: req.params.id
+    }, function(err) {
+        if (err) {
+            console.log("Error deleting article", err);
+        } else {
+            res.send();
+        }
+    });
 });
 
 
 
 // Listen on port 3000
 app.listen(3000, function() {
-  console.log("App running on port 3000!");
+    console.log("App running on port 3000!");
 });
